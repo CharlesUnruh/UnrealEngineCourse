1578323d07ddf0a9e84f98d727703826a580b3ca
Charles Unruh <cunruh3760@gmail.com>
2017-08-10 15:54:25 -0400

    Doors open, but do not close

---
+++ Section_03/Building_Escape/Source/Building_Escape/OpenDoor.cpp
@@ -0,0 +1,50 @@
+// (C) 2017 Charles Unruh
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+	
+}
+
+void UOpenDoor::OpenDoor()
+{
+	FRotator NewRotation = FRotator(0.0f, -90.0f, 0.0f);
+	GetOwner()->SetActorRotation(NewRotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		//If the actor that triggers is in the volume
+		OpenDoor();
+	}
+	
+}
+
---
+++ Section_03/Building_Escape/Source/Building_Escape/OpenDoor.h
@@ -0,0 +1,39 @@
+// (C) 2017 Charles Unruh
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerVolume.h"
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(VisibleAnywhere)
+	AActor* ActorThatOpens;
+};
